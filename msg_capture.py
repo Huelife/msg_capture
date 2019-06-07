305de17145ef29fe4d748d98edd8c7942d3d303b
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-06-07 12:37:01 -0700

    Update msg_capture.py
    added sentence parts variable lists

--- msg_capture.py
+++ msg_capture.py
@@ -1,1 +1,11 @@
 #msg_capture.py: Replies to user input and stores unknown words
+
+#sentence parts variables
+verb = ["run","walk","jog","sprint"]
+adverb = []
+noun = []
+pronoun = []
+adjective = []
+prepositon = []
+conjunction = []
+interjection = []
